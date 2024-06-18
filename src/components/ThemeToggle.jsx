4fb03d98a889f2c0b3ef92718e67d7066d77c669
Md Alamin <alaminduc516@gmail.com>
2024-06-19 03:59:11 +0600

    refactor: Admin Navbar

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,7 +1,6 @@
 import { useTheme } from 'next-themes';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import Brightness2Icon from '@mui/icons-material/Brightness2';
 import { useEffect, useState } from 'react';
+import { Moon, Sun } from 'lucide-react';
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
@@ -16,13 +15,13 @@ const ThemeToggle = () => {
   return (
     <div className="cursor-pointer p-2 hover:bg-yellow-500 dark:hover:bg-gray-700 rounded-full transition-all duration-300 ease-in-out">
       {theme === 'light' ? (
-        <Brightness2Icon
+        <Moon
           className="h-6 w-6 dark:text-gray-300 animate-slideInFromTop"
           onClick={() => setTheme('dark')}
           aria-label="Toggle Dark Mode"
         />
       ) : (
-        <WbSunnyIcon
+        <Sun
           className="h-6 w-6 text-yellow-500 animate-slideInFromTop"
           onClick={() => setTheme('light')}
           aria-label="Toggle Light Mode"
--- src/components/admin/AdminNavbar.jsx
+++ src/components/admin/AdminNavbar.jsx
@@ -1,9 +1,127 @@
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import CancelIcon from "@mui/icons-material/Cancel";
 import MenuIcon from "@mui/icons-material/Menu";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import { useTheme } from "next-themes";
+import {
+  Bell,
+  LayoutDashboard,
+  LogOut,
+  Settings,
+  UserCircle,
+} from "lucide-react";
+import Image from "next/image";
 import ThemeToggle from "../ThemeToggle";
+
+const notifications = (
+  <>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center">
+        <Bell className="h-6 w-6" />
+        <span class="sr-only">Notifications</span>
+        <div class="absolute inline-flex items-center justify-center w-5 h-5 text-[10px] font-bold text-white bg-red-700 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
+          20
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-10 lg:mr-4">
+        <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="flex space-x-4 justify-between items-center">
+          <Image
+            width={200}
+            height={200}
+            src={"/images/alamin.png"}
+            className="w-8 h-8 rounded-full"
+          />
+          <div className="flex flex-col space-y-1">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <div className="flex items-center space-x-2 text-xs">
+              <p className="px-2 py-1 bg-green-700 rounded-sm">order</p>
+              <p>Jun 19 2024 - 3:38AM</p>
+            </div>
+          </div>
+          <button className="w-6 h-6 bg-red-700 p-2 rounded-full flex items-center justify-center">x</button>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="flex space-x-4 justify-between items-center">
+          <Image
+            width={200}
+            height={200}
+            src={"/images/alamin.png"}
+            className="w-8 h-8 rounded-full"
+          />
+          <div className="flex flex-col space-y-1">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <div className="flex items-center space-x-2 text-xs">
+              <p className="px-2 py-1 bg-green-700 rounded-sm">order</p>
+              <p>Jun 19 2024 - 3:38AM</p>
+            </div>
+          </div>
+          <button className="w-6 h-6 bg-red-700 p-2 rounded-full flex items-center justify-center">x</button>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="flex space-x-4 justify-between items-center">
+          <Image
+            width={200}
+            height={200}
+            src={"/images/alamin.png"}
+            className="w-8 h-8 rounded-full"
+          />
+          <div className="flex flex-col space-y-1">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <div className="flex items-center space-x-2 text-xs">
+              <p className="px-2 py-1 bg-green-700 rounded-sm">order</p>
+              <p>Jun 19 2024 - 3:38AM</p>
+            </div>
+          </div>
+          <button className="w-6 h-6 bg-red-700 p-2 rounded-full flex items-center justify-center">x</button>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="flex space-x-4 justify-between items-center">
+          <Image
+            width={200}
+            height={200}
+            src={"/images/alamin.png"}
+            className="w-8 h-8 rounded-full"
+          />
+          <div className="flex flex-col space-y-1">
+            <p>Lorem ipsum dolor sit amet...</p>
+            <div className="flex items-center space-x-2 text-xs">
+              <p className="px-2 py-1 bg-red-700 rounded-sm">stock out</p>
+              <p>Jun 19 2024 - 3:38AM</p>
+            </div>
+          </div>
+          <button className="w-6 h-6 bg-red-700 p-2 rounded-full flex items-center justify-center">x</button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </>
+);
+
+const dropdown = (
+  <>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex items-center">
+        <UserCircle className="h-6 w-6 lg:h-8 lg:w-8" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-10 lg:mr-4">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="flex gap-1">
+          <LayoutDashboard className="h-4 w-4" /> Dashboard
+        </DropdownMenuItem>
+        <DropdownMenuItem className="flex gap-1">
+          <Settings className="h-4 w-4" /> Edit Profile
+        </DropdownMenuItem>
+        <DropdownMenuItem className="flex gap-1">
+          <LogOut className="h-4 w-4" /> Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </>
+);
 
 const AdminNavbar = ({ handleMenu, display, handleMinimize, minimize }) => {
   return (
@@ -31,29 +149,19 @@ const AdminNavbar = ({ handleMenu, display, handleMinimize, minimize }) => {
         </span>
       </div>
 
-      <div className="lg:pr-5 flex space-x-2 lg:space-x-4 items-center">
-        <ThemeToggle/>
-        <div className="cursor-pointer">
-          <NotificationsIcon className="h-6 w-6" />
+      <div className="lg:pr-5 flex space-x-3 lg:space-x-4 items-center">
+        <ThemeToggle />
+        <div className="cursor-pointer relative ">{notifications}</div>
+        <div className="block lg:hidden">
+          <div className="cursor-pointer p-2">{dropdown}</div>
         </div>
         <div className="block lg:hidden">
-          <div className="cursor-pointer">
-            <AccountCircleIcon className="h-10 w-10" />
-          </div>
-        </div>
-        <div className="block lg:hidden">
-          <div className="cursor-pointer" onClick={handleMenu}>
-            {display ? (
-              <CancelIcon className={`h-10 w-10`} />
-            ) : (
-              <MenuIcon className={`h-10 w-10`} />
-            )}
+          <div className="cursor-pointer p-2" onClick={handleMenu}>
+            {display ? <CancelIcon /> : <MenuIcon />}
           </div>
         </div>
         <div className="hidden lg:block">
-          <div className="cursor-pointer">
-            <AccountCircleIcon className="h-10 w-10" />
-          </div>
+          <div className="cursor-pointer p-2">{dropdown}</div>
         </div>
       </div>
     </nav>
